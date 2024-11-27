7624d140b970017ad4cfa00e05f383f27b6cce00
leojalo <ljalo2008@gmail.com>
2024-11-27 10:06:11 +0200

    Add files via upload

---
+++ Jens shoppingvagn.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<string> matlista = new List<string>();
+        string input;
+
+        Console.WriteLine("Skriv vad du lägga till i matlistan (skriv 'slut' för att avsluta):");
+
+        while (true)
+        {
+            input = Console.ReadLine();
+
+            if (input.ToLower() == "slut")
+            {
+                break;
+            }
+
+            matlista.Add(input);
+        }
+
+        Console.WriteLine("\nDin matlista:");
+        for (int i = 0; i < matlista.Count; i++)
+        {
+            Console.WriteLine(matlista[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
